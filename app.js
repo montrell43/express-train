75b7dbb49cc8a91f5db61f909b803105350ccd59
montrell43 <tmerriweather@persevernow.org>
2025-07-24 11:27:45 -0500

    add commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const storeRouter = require("./routes/storeRouter");
 // const logger = require('morgan');
 
 const app = express(); // app represents the server
@@ -7,57 +8,14 @@ const app = express(); // app represents the server
 app.use(morgan("dev")); // this will logg the incoming requests// npm morgan in the search bar to visit  package site
 app.use(express.json());
 
-let store = [
 
-    {
-        name: 'apple',
-        price: 1.5
-    }
-
-];
 
 app.get('/', (req, res) => { // how does the server handles a request to '/'
-   res.json(store);
+   res.json("Welcome to my store");
 }) // request type
 
-// Get
-app.get('/get-all-products',(req, res) => {
-    let names = [];
+app.use("/", storeRouter);
 
-    for (let i = 0; i < store.length; i++) {
-        names.push(store[i].name);
-    }
-    res.json(names)
-})
-
-// Post
-app.post('/create-product', (req, res) => {
-    //console.log(req.body)
-    const found = store.find(product => product.name === req.body.name) //grab the body
-    if(found) {
-        res.end("Product already exists")
-    } else {
-        store.push({ name: req.body.name, price: req.body.price });
-        res.end("Product added")
-    }
-    //data.push({...req.body, id: uuidv4()}) // spread operators
-    //res.end({message:"Team Created"})
-});
-
-// PUT -updated
-app.put('/update-product/:name', (req, res) => {
-//console.log(req.params.name)
-// id => find the team with that id
-const foundProduct = store.find(product => product.name === req.params.name)
-//console.log(foundTeam)
-if(!foundProduct){
-    res.end("Product not found");
-} else {
- foundProduct.price = req.body.price;
-    // edit info
- res.end("Product Updated")
-}
-})
 
 app.listen(3000, () => { // starts listening to the port we give it
     console.log("Server Started on port 3000");
---
+++ routes/storeRouter.js
@@ -0,0 +1,64 @@
+const express = require('express');
+//const morgan = require('morgan');
+const router = express.Router();
+
+let store = [
+
+    {
+        name: 'apple',
+        price: 1.5
+    }
+
+];
+
+// Get
+router.get('/get-all-products',(req, res) => {
+    let names = [];
+
+    for (let i = 0; i < store.length; i++) {
+        names.push(store[i].name);
+    }
+    res.json(names)
+})
+
+// Post
+router.post('/create-product', (req, res) => {
+    //console.log(req.body)
+    const found = store.find(product => product.name === req.body.name) //grab the body
+    if(found) {
+        res.end("Product already exists")
+    } else {
+        store.push({ name: req.body.name, price: req.body.price });
+        res.end("Product added")
+    }
+    //data.push({...req.body, id: uuidv4()}) // spread operators
+    //res.end({message:"Team Created"})
+});
+
+// PUT -updated
+router.put('/update-product/:name', (req, res) => {
+//console.log(req.params.name)
+// id => find the team with that id
+const foundProduct = store.find(product => product.name === req.params.name)
+//console.log(foundTeam)
+if(!foundProduct){
+    res.end("Product not found");
+} else {
+ foundProduct.price = req.body.price;
+    // edit info
+ res.end("Product Updated")
+}
+})
+
+// Delete
+router.delete("/delete-product/:name", (req, res) => {
+    const newArray = data.filter(name => name === req.params.name);
+    if (newArray === -1) {
+        res.end("Product not found")
+    } else {
+        store.splice(newArray, 1);
+        res.end("Product Deleted")
+    };
+})
+
+module.exports = router;
